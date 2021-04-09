90992665749e214b548ef8cdf02a9b565dc9102b
Ga11eaN <manyukhy@gmail.com>
2021-04-09 19:09:44 +0300

    Created birthday function. Updated contacts.

--- ProjectHelper/contacts_list.py
+++ ProjectHelper/contacts_list.py
@@ -1,14 +1,43 @@
 from collections import UserDict, UserString, UserList
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class ContactList(UserDict):
     
     def add_contact(self, record):
-        self.data[record.name] = {}
-        self.data[record.name]['address'] = record.address
-        self.data[record.name]['phone'] = record.phone
-        self.data[record.name]['mail'] = record.mail
-        self.data[record.name]['birthday'] = record.birthday
+        self.data[record.name] = record.data
+        
+    #calculate contacts with birthdays from days_from to days_to
+    def birthdays(self, days_from, days_to):
+    
+        if not(isinstance(days_from, int) and isinstance(days_to, int)) or days_from < 0 or days_to < 0 or days_to < days_from:
+            return f'Input is not correct. Days should be integer and days from should be less then days to'
+    
+        date_now = datetime.now()
+        delta_from = timedelta(days = days_from)
+        delta_to = timedelta(days = days_to)
+        
+        #dates from - to in datetime
+        date_from = date_now + delta_from
+        date_to = date_now + delta_to
+        
+        finded_contacts = f'Finded contacts with birthdays in range:\n'
+        for key, value in self.data.items():
+            
+            #find correct birthday date between from and to
+            if date_from.month < value['birthday'].month:
+                birthday_date = value['birthday'].date.replace(year = date_now.year)
+            elif date_from.month == value['birthday'].month:
+                if date_from.day <= value['birthday'].day:
+                    birthday_date = value['birthday'].date.replace(year = date_now.year)
+                else:
+                    birthday_date = value['birthday'].date.replace(year = date_now.year + 1)
+            else:
+                birthday_date = value['birthday'].date.replace(year = date_now.year + 1)
+                
+            if date_from < birthday_date < date_to:
+                finded_contacts += f'{key}: {self.data[key]}\n'
+                
+        return finded_contacts
 
 class Birthday(UserString):
     
@@ -27,45 +56,83 @@ class Birthday(UserString):
             self.__data = new_value
         except ValueError:
             self.__data = ''
-            print('Birthday input is not correct. Should be in format: DD/MM/YYYY')
-
-class Address(UserList):
+            print(f'Birthday input is not correct. Should be in format: DD.MM.YYYY')
     
-    def __init__(self, value):
-        self.data = [value]
-
-class Mail(UserList):
+class Record(UserDict):
     
-    def __init__(self, value):
-        self.data = [value]
-
-class Phone(UserList):
-    
-    def __init__(self, value):
-        self.data = [value]
-    
-class Record:
-    
-    def __init__(self, name, address = '', phone = '', mail = '', birthday = ''):
-        self.name = name
-        self.address = Address(address)
-        self.phone = Phone(phone)
-        self.mail = Mail(mail)
-        self.birthday = Birthday(birthday)
-
-    def add_phone(self, value):
-        self.phone.data.append(value)
+    #creating Record dict with all values
+    def __init__(self):
+        self.data = {}
+        self.add_name()
+        self.add_value('address')
+        self.add_value('phone')
+        self.add_value('mail')
+        self.add_birthday()
         
-    def del_phone(self):
-        self.phone.clear()
+    #adding name attribute to the Record
+    def add_name(self):
+        user_input = input('Enter your name please: ')
+        self.name = user_input
+            
+    
+    #adding phone, mail and address to the Record
+    def add_value(self, value):
+        while True:
+           
+            user_input = input(f'Enter the {value} please: ')
+                    
+            if value == 'phone':
+                if not self.__phone_check(user_input):
+                    continue
+            
+            if value == 'mail':
+                if not self.__mail_check(user_input):
+                    continue
+            
+            if value not in self.data.keys():
+                self[value] = [user_input]
+            else:
+                self[value].append(user_input)
 
-
+            add_more = input(f'Do you want to enter another {value}? ')
+            if add_more == 'yes':
+                continue
+            else:
+                break
+    
+    #phone check
+    def __phone_check(self, phone):
+        if (phone[:3] == '380' and len(phone) == 12) or not phone:
+            return True
+        else:
+            print('Phone number is not correct. Should start from 380 and have 12 digits')
+            return False
+    
+    #mail check
+    def __mail_check(self, mail):
+        #if mail[:3] == '380' and len(mail) == 12:
+        return True
+        #else:
+        #    print('Mail is not correct.')
+        #    return False
+    
+    #birthday adding
+    def add_birthday(self):
+        while True:
+           
+            user_input = input('Enter the birthday please in format "DD.MM.YYYY": ')
+            self.data['birthday'] = Birthday(user_input)
+            
+            if self.data['birthday'] or not user_input:
+                break
+    
+    
 a = ContactList()
 
-b = Record('Ivan', address = 'Mazepy 28', phone = '0936795412', mail = 'ivan75@gmail.com', birthday = '13/02/2000')
+b = Record()
 
 a.add_contact(b)
-b.add_phone('0674712255')
+#a.add_contact(c)
 
 
-print(a.data)
+print(a)
---
+++ ProjectHelper/main.py
@@ -0,0 +1,16 @@
+from contacts_list import *
+
+
+def main:
+    while True:
+        user_input = input('What next:')
+        if user_input == 'exit':
+            print('Bye!')
+            break
+            
+        
+        
+
+
+if __name__ == '__main__':
+    main()
