007acf5fde80e57e100961575ff8823ca7c3b798
Ga11eaN <manyukhy@gmail.com>
2021-04-10 16:14:46 +0300

    Added save and load to csv file

--- ProjectHelper/contacts_list.py
+++ ProjectHelper/contacts_list.py
@@ -1,13 +1,52 @@
 from collections import UserDict, UserString, UserList
 from datetime import datetime, timedelta
 from re import fullmatch
-
+import csv
 
 class ContactList(UserDict):
-    
+
     def add_contact(self, record):
-        self.data[record.name] = record.data
+        self.data[record.name] = record
+
+    #find value in ContactList
+    def find(self, find_value):
+        finded_names = ''
         
+        for key, value in self.data.items():
+            if find_value in key:
+                finded_names += f'{key}: {self.data[key]}\n'
+                continue
+            
+            search_values = value['address'] + value['phone'] + value['mail'] + [value['birthday']]
+
+            for i in value:
+                if find_value in i:
+                    finded_names += f'{key}: {self.data[key]}\n'
+                    break
+                
+        if not finded_names:
+            print('No value {find_value} found')
+        else:
+            print(finded_names)
+
+    #change name in ContactList
+    def change_name(self):
+        while True:
+            user_input = input('Please enter name to rename: ')
+            if user_input not in self.data.keys():
+                user_input2 = input('Entered name is not in the contact book.\n1. Try again\n2.return to previous menu?\n')
+                if user_input2 == '1':
+                    continue
+                else:
+                    break
+            else:
+                new_input = input('Enter new name: ')
+                self.data[new_input] = self.data.pop(user_input)
+                self.data[new_input].name = new_input
+                break
+
+            
+       
     #calculate contacts with birthdays from days_from to days_to
     def birthdays(self, days_from, days_to):
     
@@ -41,6 +80,37 @@ class ContactList(UserDict):
                 
         return finded_contacts
 
+    #save contact list
+    def save_csv(self):
+        user_input = input('Enter the name of your Contact List: ')
+        with open(f'{user_input}.csv', 'w', newline='') as file:
+            field_names = ['name', 'phone', 'address', 'mail', 'birthday']
+            writer = csv.DictWriter(file, fieldnames=field_names)
+            writer.writeheader()
+            for key,value in self.data.items():
+                dict_to_write = value
+                dict_to_write['name'] = key
+                dict_to_write['phone'] = '&'.join(dict_to_write['phone'])
+                dict_to_write['phone'] = dict_to_write['phone'].replace("'",'')
+                dict_to_write['address'] = '&'.join(dict_to_write['address'])
+                dict_to_write['address'] = dict_to_write['address'].replace("'",'')
+                dict_to_write['mail'] = '&'.join(dict_to_write['mail'])
+                dict_to_write['mail'] = dict_to_write['mail'].replace("'",'')
+                writer.writerow(dict_to_write)
+
+    def load(self):
+        user_input = input('Enter the name of your Contact List: ')
+        with open(f'{user_input}.csv', newline='') as file:
+            reader = csv.DictReader(file)     
+            for row in reader:
+                key = row.pop('name')
+                row['mail'] = row['mail'].split('&')
+                row['phone'] = row['phone'].split('&')
+                row['address'] = row['address'].split('&')
+                self.data[key] = row
+                
+        
+
 class Birthday(UserString):
     
     @property
@@ -49,7 +119,7 @@ class Birthday(UserString):
         
     @data.setter
     def data(self, new_value):
-
+        
         try:
             self.year = int(new_value[6:])
             self.month = int(new_value[3:5])
@@ -58,7 +128,8 @@ class Birthday(UserString):
             self.__data = new_value
         except ValueError:
             self.__data = ''
-            print(f'Birthday input is not correct. Should be in format: DD.MM.YYYY')
+            if new_value:
+                print(f'Birthday input is not correct. Should be in format: DD.MM.YYYY')
     
 class Record(UserDict):
     
@@ -66,8 +137,8 @@ class Record(UserDict):
     def __init__(self):
         self.data = {}
         self.add_name()
-        self.add_value('address')
         self.add_value('phone')
+        self.add_value('address')
         self.add_value('mail')
         self.add_birthday()
         
@@ -81,7 +152,7 @@ class Record(UserDict):
     def add_value(self, value):
         while True:
            
-            user_input = input(f'Enter the {value} please: ')
+            user_input = input(f"Enter the {value} please (If you don't want to enter {value}, leave empty): ")
                     
             if value == 'phone':
                 if not self.__phone_check(user_input):
@@ -132,19 +203,74 @@ class Record(UserDict):
     def add_birthday(self):
         while True:
            
-            user_input = input('Enter the birthday please in format "DD.MM.YYYY": ')
+            user_input = input('Enter the birthday please in format "DD.MM.YYYY"(If you do not want to enter birthday, leave empty): ')
             self.data['birthday'] = Birthday(user_input)
             
             if self.data['birthday'] or not user_input:
                 break
     
-    
-a = ContactList()
+    #change any value in Record except name
+    def change_value(self):
+        
+        CORRECT_INPUT = ('1', 'phone', '2', 'address', '3', 'mail', '4', 'birthday','5','back')
 
-b = Record()
+        while True:
+            user_input = input("What value do you want to change/delete?\n1. phone\n2. address\n3. mail\n4. birthday\n5.back\n")
 
-a.add_contact(b)
+            if user_input  not in CORRECT_INPUT:
+                print('Please choose correct option from the list: ')
+                continue
+            elif user_input in ('5', 'back'):
+                break
+            elif user_input in ('4', 'birthday'):
+                self.add_birthday()
+                break
+            else:
+
+                if user_input in ('1', 'phone'):
+                    value = 'phone'
+                elif user_input in ('2','address'):
+                    value = 'address'
+                else:
+                    value = 'mail'
+
+                print(self.data[value])
+
+                user_input = input(f'Choose the number of {value} to change/delete. If you want to delete all values, type "clear": ')
+
+                if user_input == 'clear':
+                    self.data[value].clear()
+                    break
+
+                data_input = input(f'Enter the correct {value} or leave empty if you want to delete {value}: ')
+
+                if value == 'phone':
+                    if not self.__phone_check(data_input):
+                        continue
+            
+                if value == 'mail':
+                    if not self.__mail_check(data_input):
+                        continue
+                if data_input:
+                    self.data[value][int(user_input)-1] = data_input
+                else:
+                    self.data[value].pop(int(user_input)-1)
+                break
+
+
+
+
+#a = ContactList()
+
+#b = Record()
+#c = Record()
+
+#a.add_contact(b)
 #a.add_contact(c)
 
+#print(a)
+#a.save_csv()
 
-print(a)
+g = ContactList()
+g.load()
+print(g)
--- ProjectHelper/main.py
+++ ProjectHelper/main.py
@@ -1,15 +1,13 @@
 from contacts_list import *
 
 
-def main:
+def main():
     while True:
-        user_input = input('What next:')
+        user_input = input("Hello, I'm your personal helper. What can I do for you today?:\n1. Load contact's list\n2. Create new contact's list\n\
+3. Work with notes\n4. Sort your folders")
         if user_input == 'exit':
             print('Bye!')
             break
-            
-        
-        
 
 
 if __name__ == '__main__':
